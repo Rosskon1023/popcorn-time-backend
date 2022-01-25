39f54584fff8357a60f714f644669079695d7497
rkonesky <rosskon1023@gmail.com>
2022-01-24 20:21:06 -0600

    backend: added basic boilerplate for server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+// Require Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+
+// Add Controllers
+
+
+// Initialize the Application
+const app = express();
+
+
+// Configure Application Settings
+const {PORT, DATABASE_URL} = process.env;
+
+
+// Connect to and Configure the Database
+mongoose.connect(DATABASE_URL);
+
+
+// Set up Database Listeners
+mongoose.connection
+    .on("open", () => console.log("Connected to MongoDB"))
+    .on("close", () => console.log("Disconnected from MongoDB"))
+    .on("error", () => console.log(error))
+
+// Mount Middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+// Mount Routes
+
+
+// Application Listener
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
